c7a7f48f5cf85179430c759eaab8f8109653d518
Redowan0019 <146367158+Redowan0019@users.noreply.github.com>
2024-06-29 13:40:01 +0600

    LinkList ( 29/06/24 )...

--- DataStructure/LinkList.c
+++ DataStructure/LinkList.c
@@ -6,6 +6,7 @@ struct node {
     struct node *next;
 } *head = NULL;
 
+
 struct node *createNewNode(int x) {
     struct node *newNode = (struct node*)malloc(sizeof(struct node));
     newNode -> data = x;
@@ -13,14 +14,15 @@ struct node *createNewNode(int x) {
     return newNode; 
 };
 
+
 void insertBeginning(int key) {
     struct node *newNode = createNewNode(key);
     newNode -> next = head;
     head = newNode;
 }
 
-void insertLast(int key)
-{
+
+void insertLast(int key) {
     struct node *newNode = createNewNode(key);
     // If the list is empty...
     if (head == NULL) {
@@ -36,86 +38,118 @@ void insertLast(int key)
     }
 }
 
-void insertAt(int key,int pos)
-{
-    if(pos<0)
-    {
+
+void insertAt(int key, int pos) {
+    if (pos < 0) {
         printf("invalid position\n");
         return;
     }
-    else if(pos==0)
-    {
+    else if (pos == 0) {
         insertBeginning(key);
         return;
     }
     else
     {
-        int current_position=0;
-        struct node *current=head;
-        while(current!=NULL && current_position< pos-1)
+        int current_position = 0;
+        struct node *current = head;
+
+        while(current != NULL && current_position < pos-1)
         {
-            current=current->next;
+            current = current -> next;
             current_position++;
         }
 
-        if(current!=NULL)
-        {
-            struct node *newnode=createNewNode(key);
-            newnode->next=current->next;
-            current->next=newnode;
+        // current is the node at (pos - 1)...
+        if(current != NULL) {
+            struct node *newnode = createNewNode(key);
+            newnode -> next = current -> next;
+            current -> next = newnode;
         }
-        else
-        {
+        else {
             printf("Position exceeds the length of the list. %d is not inserted.\n",key);
         }
     }
-
-}
-void deleteFirst()
-{
-
 }
 
-void deleteLast()
-{
 
+void deleteFirst() {
+    if (head == NULL) {
+        printf("The list is empty.");
+        return;
+    }
+
+    struct node *t = head;
+    head = head -> next;
+    free(t);    
 }
-void deleteAt(int pos)
-{
-    if(pos<0)
-    {
+
+
+void deleteLast() {
+    struct node *t = head;
+
+    if (head == NULL) {
+        printf("List is empty.");
+    }
+    else if (head -> next == NULL) {
+        head = NULL;
+        free(t);
+    }
+    else {
+        while (head -> next -> next != NULL) {
+            t = t -> next;
+        }
+
+        // t is the 2nd last node. Last node is  t -> next...
+        free(t -> next); // Delete last node...
+        t -> next = NULL; // Set the next of 2nd last node as NULL...
+    }
+}
+
+
+void deleteAt(int pos) {
+    if(pos < 0) {
         printf("Invalid position.\n");
     }
-    else if(pos==0)
-    {
+    else if (pos == 0) {
         deleteFirst();
     }
-    else
-    {
-        int current_position=0;
-        struct node *current=head;
-        while(current!=NULL && current_position<pos-1)
-        {
-            current=current->next;
+    else {
+        int current_position = 0;
+        struct node *current = head;
+
+        while (current != NULL && current_position < pos - 1) {
+            current = current -> next;
             current_position++;
         }
-        if(current==NULL)
-        {
-            printf("Position exceeds the length of the list. Can't be deleted at position %d.\n",pos);
 
+        if(current == NULL) {
+            printf("Position exceeds the length of the list. Can't be deleted at position %d.\n",pos);
         }
-        else
-        {
-            struct node *t=current->next;
-            current->next=current->next->next;
+        else {
+            struct node *t = current -> next;
+            current -> next = t -> next;
             free(t);
         }
     }
 }
 
-int search(int x){
 
+int search(int x){
+    int current_position = 0;
+    struct node *t = head;
+
+    while (t != NULL) {
+        if (t -> data == x) {
+            return current_position;
+        }
+        else {
+            current_position++;
+             t = t -> next;
+        }
+    }
+    return -1;
 }
+
 
 void printList() {
     printf("LinkList: ");
@@ -129,26 +163,28 @@ void printList() {
     printf("\n");
 }
 
+
 int main() {
     int choice,key,pos;
-    printf("1.insert first 2.insert last 3.insert at\n");
-    printf("4.delete first 5.delete last 6.delete at 7.search 8.exit\n");
-    while(1){
+    printf("1.insert first, 2.insert last, 3.insert at, 4.delete first, 5.delete last, 6.delete at, 7.search, 8.exit\n\n");
 
-        printf("enter choice:");
-        scanf("%d",&choice);
-        if(choice==1){
-            printf("enter element:");
-            scanf("%d",&key);
+    while(1){
+        printf("enter choice: ");
+        scanf("%d", &choice);
+        if(choice == 1) {
+            printf("enter element: ");
+            scanf("%d", &key);
             insertBeginning(key);
-        }else if(choice==2){
-            printf("enter element:");
-            scanf("%d",&key);
+        }
+        else if(choice == 2) {
+            printf("enter element: ");
+            scanf("%d", &key);
             insertLast(key);
-        }else if(choice==3){
-            printf("enter position:");
+        }
+        else if(choice==3){
+            printf("enter position: ");
             scanf("%d",&pos);
-            printf("enter element:");
+            printf("enter element: ");
             scanf("%d",&key);
             insertAt(key,pos);
         }else if(choice==4){
@@ -156,11 +192,11 @@ int main() {
         }else if(choice==5){
             deleteLast();
         }else if(choice==6){
-            printf("enter position:");
+            printf("enter position: ");
             scanf("%d",&pos);
             deleteAt(pos);
         }else if(choice==7){
-            printf("enter element:");
+            printf("enter element: ");
             scanf("%d",&key);
             int idx=search(key);
             if(idx==-1){

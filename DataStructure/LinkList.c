726c8827c0be440dd2013b7028a3e7345e2157db
Redowan0019 <146367158+Redowan0019@users.noreply.github.com>
2024-06-29 23:13:49 +0600

    LinkLIst (29/06/24)

--- DataStructure/LinkList.c
+++ DataStructure/LinkList.c
@@ -66,7 +66,7 @@ void insertAt(int key, int pos) {
             current -> next = newnode;
         }
         else {
-            printf("Position exceeds the length of the list. %d is not inserted.\n",key);
+            printf("Position exceeds the length of the list. %d is not inserted.\n", key);
         }
     }
 }
@@ -107,7 +107,7 @@ void deleteLast() {
 
 
 void deleteAt(int pos) {
-    if(pos < 0) {
+    if (pos < 0) {
         printf("Invalid position.\n");
     }
     else if (pos == 0) {
@@ -134,7 +134,7 @@ void deleteAt(int pos) {
 }
 
 
-int search(int x){
+int search(int x) {
     int current_position = 0;
     struct node *t = head;
 
@@ -166,47 +166,62 @@ void printList() {
 
 int main() {
     int choice,key,pos;
-    printf("1.insert first, 2.insert last, 3.insert at, 4.delete first, 5.delete last, 6.delete at, 7.search, 8.exit\n\n");
+    printf("1.Insert first, 2.Insert last, 3.Insert at, 4.Delete first, 5.Delete last, 6.Delete at, 7.Search, 8.Exit\n\n");
 
     while(1){
-        printf("enter choice: ");
+        printf("Enter choice: ");
         scanf("%d", &choice);
-        if(choice == 1) {
-            printf("enter element: ");
+
+        if (choice == 1) {
+            printf("Enter element: ");
             scanf("%d", &key);
+
             insertBeginning(key);
         }
-        else if(choice == 2) {
-            printf("enter element: ");
+        else if (choice == 2) {
+            printf("Enter element: ");
             scanf("%d", &key);
+
             insertLast(key);
         }
-        else if(choice==3){
-            printf("enter position: ");
-            scanf("%d",&pos);
-            printf("enter element: ");
-            scanf("%d",&key);
-            insertAt(key,pos);
-        }else if(choice==4){
+        else if (choice == 3) {
+            printf("Enter position: ");
+            scanf("%d", &pos);
+
+            printf("Enter element: ");
+            scanf("%d", &key);
+
+            insertAt(key, pos);
+        }
+        else if (choice == 4) {
             deleteFirst();
-        }else if(choice==5){
+        }
+        else if (choice == 5) {
             deleteLast();
-        }else if(choice==6){
-            printf("enter position: ");
-            scanf("%d",&pos);
+        }
+        else if (choice == 6) {
+            printf("Enter position: ");
+            scanf("%d", &pos);
+
             deleteAt(pos);
-        }else if(choice==7){
-            printf("enter element: ");
-            scanf("%d",&key);
-            int idx=search(key);
-            if(idx==-1){
-                printf("%d is not in the list.\n",key);
-            }else{
-                printf("The key is found at:%d\n",idx);
+        }
+        else if (choice == 7) {
+            printf("Enter element: ");
+            scanf("%d", &key);
+
+            int idx = search(key);
+
+            if (idx == -1) {
+                printf("%d is not in the list.\n", key);
             }
-        }else{
+            else {
+                printf("The key is found at:%d\n", idx);
+            }
+        }
+        else {
             break;
         }
+
         printList();
     }
 }
